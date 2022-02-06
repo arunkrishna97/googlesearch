193bbfad169bba8a60111ec89fefe7a4e9dd9055
Karikaalan <karikaalan.coder@gmail.com>
2022-02-06 09:21:06 +0530

    error page added

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ else if (search == "Arun")
 {
     window.location.href = "https://arunkrishna97.github.io/arun_portfolio/";
 }
+else if (search == "arun krishna")
+{
+    window.location.href = "https://arunkrishna97.github.io/arun_portfolio/";
+}
+else if (search == "arunkrishna")
+{
+    window.location.href = "https://arunkrishna97.github.io/arun_portfolio/";
+}
+else if (search == "arun")
+{
+    window.location.href = "https://arunkrishna97.github.io/arun_portfolio/";
+}
 else if (search == "Arunkrishna")
 {
     window.location.href = "https://arunkrishna97.github.io/arun_portfolio/";
@@ -17,9 +29,38 @@ else if (search == "AK")
 {
     window.location.href = "https://arunkrishna97.github.io/arun_portfolio/";
 }
+else if (search == "Dhinakar")
+{
+    window.location.href = "https://karan-github96.github.io/Dhinakar-Page/mobile-site/index.html";
+}
+else if (search == "Dhina")
+{
+    window.location.href = "https://karan-github96.github.io/Dhinakar-Page/mobile-site/index.html";
+}
+else if (search == "Karan")
+{
+    window.location.href = "https://karan-github96.github.io/Dhinakar-Page/mobile-site/index.html";
+}
+else if (search == "dhinakar")
+{
+    window.location.href = "https://karan-github96.github.io/Dhinakar-Page/mobile-site/index.html";
+}
 else
 {
-    alert("Invaid Search");
+    window.location.href = "./error.html";
 }
 
+}
+document.getElementById("lucky").onclick = function ()
+{
+    document.getElementById("searchBar").value = "Summa Edhuna Button Irundha Ammikiduviya...";
+}
+
+document.getElementById("signin").onclick = function()
+{
+    alert("Inga Sign In Panni enna panna Pora");
+}
+document.getElementById("adscr").onclick = function()
+{
+    alert("Edhukku Advance search Lam! poi vera velaiya Paaru");
 }
